46d6d927583583107c89d318c9b16bbae75c4ff8
shaunte99 <ksmatsetela99@gmail.com>
2025-01-25 23:25:49 +0200

    Add files via upload

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -4,30 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Reference reference = new Reference("Ruth", 1, 16, 17);
+        
+        Reference reference = new Reference("Ruth", 1, "16-17"); 
         string text = "And Ruth said, Intreat me not to leave thee, or to return from following after thee: " +
                       "for whither thou goest, I will go; and where thou lodgest, I will lodge: " +
                       "thy people shall be my people, and thy God my God. " +
                       "Where thou diest, will I die, and there will I be buried: " +
                       "the Lord do so to me, and more also, if ought but death part thee and me.";
 
-    
         Scripture scripture = new Scripture(reference, text);
 
-        
         while (true)
         {
-        
             scripture.Display();
 
-    
             if (scripture.AreAllWordsHidden())
             {
                 Console.WriteLine("Good job!! you have memorized your scripture");
                 break;
             }
 
-            
             Console.WriteLine("Press Enter to hide words or type \"quit\" to exit");
             string input = Console.ReadLine();
 
@@ -37,7 +33,6 @@ class Program
                 break;
             }
 
-            
             scripture.HideRandomWords(3);
         }
     }
