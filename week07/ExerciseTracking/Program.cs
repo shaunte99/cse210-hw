48e0aec511f6649ec82ff22976328786d6f9e25e
shaunte99 <ksmatsetela99@gmail.com>
2025-02-18 19:55:04 +0200

    Update Program.cs
    Program main class

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,25 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+public class Program
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Activity> activities = new List<Activity>
+        {
+            new Running(DateTime.Parse("2025-02-15"), 30, 5.0),
+            new Cycling(DateTime.Parse("2025-02-14"), 45, 20.0),
+            new Swimming(DateTime.Parse("2025-02-13"), 60, 40)
+        };
+
+        Console.WriteLine("** Mzansi Fitness Tracker - Powered by Kamogelo Shaun Matsetela **\n");
+        
+        foreach (var activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+            Console.WriteLine(new string('-', 60));
+        }
+
+      Console.WriteLine("\nHamba kahle! Keep fit and stay lekker!");
     }
 }
